9e67496b3d6747a896de272785ab29aa554cb258
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-02-19 23:13:24 -0800

    create a new target value and reset the current value for new rounds
    17. Writing Methods (for DRY and extensibility)

--- Bull's Eye/Bull's Eye/ViewController.swift
+++ Bull's Eye/Bull's Eye/ViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Starting value
     var currentValue: Int = 0
+    // Targe value for the user/player
+    var targetValue: Int = 0
     
     @IBOutlet weak var slider: UISlider!
     
@@ -18,24 +21,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
         
-        let message = "The value of the slider is now: \(currentValue)"
+        let alertMessage =
+        """
+        The value of the slider is now: \(currentValue)
+        The target value is: \(targetValue)
+        """
         
-        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Hello, World!", message: alertMessage, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         // Adds the button to the alert
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
+        startNewRound()
+        
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         let roundedValue = slider.value.rounded()
-        currentValue = Int(slider.value)
+        currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        
+        // Override the value set in the main storyboard
+        currentValue = 50
+        // Reset slider thumb position
+        slider.value = Float(currentValue)
     }
     
 }
