19735f38e033310ab6a35ceb1c5cbca2b6647b8a
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-03-04 15:34:49 -0800

    add info view controller and the ability to dismiss it
    some files were relocated

---
+++ Bull's Eye/Bull's Eye/View Controllers/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  Bull's Eye
+//
+//  Created by Jaspal on 3/4/19.
+//  Copyright © 2019 Jaspal Suri. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
