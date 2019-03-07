943078d7120b2417d7345d889c667141a64fac17
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-03-06 16:24:55 -0800

    test a different algorithm for bull's eye
    - explore the features offered by Playgrounds

--- Programming in Swift/Hello Playgrounds.playground/Contents.swift
+++ Programming in Swift/Hello Playgrounds.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var str = "Hello, playground"
+var string = "Hello, playground"
+string = "Hi!"
+
+// You can use cmd+shift+enter to run the playground
+// This is not mentioned in the video. :)
+
+var currentValue = 50
+var targetValue = 55
+
+var difference = currentValue - targetValue
+
+if difference < 0 {
+    difference *= -1
+}
+
+print(difference)
+
+
