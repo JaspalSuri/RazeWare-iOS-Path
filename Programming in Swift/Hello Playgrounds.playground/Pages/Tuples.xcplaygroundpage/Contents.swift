e92cd897169ca3a8ede0469fc2d9da86fd4d8440
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-03-06 17:02:25 -0800

    tuples overview

--- Programming in Swift/Hello Playgrounds.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
+++ Programming in Swift/Hello Playgrounds.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
 //: [Previous](@previous)
 
-import Foundation
+// First tuple
 
-var str = "Hello, playground"
+let coordinates: (Int, Int) = (2, 3)
+
+let coordinatesDoubles = (2.3, 21.3)
+
+let coodinatesMixed = (2.1, 3, "random string")
+
+let x1 = coordinates.0
+let y1 = coordinates.1
+
+
+let coordinatesNamed = (x: 2, y: 3)
+
+
+let x2 = coordinatesNamed.x
+let y2 = coordinatesNamed.y
+
+let coordinates3D = (x: 2, y: 3, z: 1)
+
+let (x3, y3, z3) = coordinates3D
+x3
+y3
+z3
+
+let (x4, y4, _) = coordinates3D
+x4
+y4
+// z4 would not work
+
 
 //: [Next](@next)
