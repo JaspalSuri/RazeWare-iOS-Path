5224bfcbd123117bafb539a70087bcda23de6358
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-03-06 16:39:33 -0800

    update comments file
    Doesn't contain examples, since I'm already aware of them

--- Programming in Swift/Hello Playgrounds.playground/Pages/Comments.xcplaygroundpage/Contents.swift
+++ Programming in Swift/Hello Playgrounds.playground/Pages/Comments.xcplaygroundpage/Contents.swift
@@ -1,3 +1,9 @@
 import Foundation
 
 var str = "Hello, playground"
+
+// You can make nested comments
+// You can create multi-line comments
+// You can set the render markup option to a keyboard shortcut in XCode's settings. Currently it's set to Control+R.
+// Documentation on Markup
+// https://developer.apple.com/library/archive/documentation/Xcode/Reference/xcode_markup_formatting_ref/index.html
