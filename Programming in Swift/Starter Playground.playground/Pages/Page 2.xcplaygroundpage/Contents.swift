f02f11df0c95a7f02ab42053d478e66e852bb690
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-05-07 16:38:52 -0700

    using multiline, nested, and single line comments

---
+++ Programming in Swift/Starter Playground.playground/Pages/Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+
+// Close the navigator with cmd+0
+
+/*
+ 
+ Multiline comments make life easier
+    /* nested comments are also useful, sort of like creating an outline */
+ 
+ Use Control+R to render the links to pages, as it was what was set in the video and seems intuitive.
+ 
+ */
