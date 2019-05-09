31db8a3122f142393ddd51dc7b24e7c30e69e6d7
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-05-08 19:58:52 -0700

    changes to Booleans and Operators challenge
    1. correct as expected
    2. I replaced the left hand side with the variable that evaluated and was equated to whether or not my given age classified me as a teenager, and added parentheses for readability on the right hand side, as I did in my notes but failed to implement in the exercises.
    3. My use of tuples saved a line
    4. Was simple and worked as expected.
    5. Should've used the isTeenager constant for DRY
    6. I could have figured out number 6 if I had used !(...) and switched the two values after the condition. The lack of use of parentheses in Swift often throws me off. To make things more efficient and intuitive for the exercise, all I had to do was use !isTeenager for my condition. I also have to print the answer, as a project will not do that automatically.

--- Programming in Swift/Core Concepts/07 - Challenge - Booleans/PS_1-07_Start.playground/Contents.swift
+++ Programming in Swift/Core Concepts/07 - Challenge - Booleans/PS_1-07_Start.playground/Contents.swift
@@ -40,7 +40,7 @@ let isTeenager = myAge >= 13 && myAge <= 19
  Create another constant called `leonardosAge` and set it to 14, the age of Leonardo in the 1984 Teenage Mutant Ninja Turtles comic. Then, create a constant called `eitherAreTeenagers` that uses Boolean logic to determine if either you or Leonardo are teenagers.
  */
 let leonardosAge = 14
-let eitherAreTeenagers = myAge >= 13 && myAge <= 19 || leonardosAge >= 13 && leonardosAge <= 19
+let eitherAreTeenagers = isTeenager || (leonardosAge >= 13 && leonardosAge <= 19)
 /*:
  Create a constant called student and set it to your name as a string. Create a constant called author and set it to "Matt Galloway", the original author of these exercises. Create a constant called `authorIsStudent` that uses string equality to determine if student and author are equal.
  */
@@ -54,7 +54,7 @@ let studentBeforeAuthor = student <= author
  ### IF STATEMENTS AND BOOLEANS
  You've already created a constant called "myAge" and checked to see if you are a teenager. Using that information, write an if statement to print out "Teenager" if you're a teenager, and "Not a teenager" if you're not.
  */
-if myAge >= 13 && myAge <= 19 {
+if isTeenager {
     print("Teenager")
 } else {
     print("Not a teenager")
@@ -62,4 +62,5 @@ if myAge >= 13 && myAge <= 19 {
 /*:
  Create a constant called `answer` and use a ternary conditional operator to check if you are *not* a teenager and set it equal the same strings you printed in the above exercise. Then print out answer.
  */
-let answer = myAge >= 13 && myAge <= 19 ? "Teenager" : "Not a teenager"
+let answer = !isTeenager ? "Not a teenager" : "Teenager"
+print(answer)
