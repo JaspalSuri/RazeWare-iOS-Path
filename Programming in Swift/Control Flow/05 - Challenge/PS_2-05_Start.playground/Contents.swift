6605ece272b721f5fe67a306f73b3823aa02cc8a
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-05-09 16:59:33 -0700

    after for loop challenge review
    1. My code is shorter, but it would be better to assign the square to a variable if I needed to use it in the future.
    2. I could have checked if the remainder was odd, since that would be more precise in a way. Also using a comparison operator would be a nice way of knowing if I had achived the desired result

--- Programming in Swift/Control Flow/05 - Challenge/PS_2-05_Start.playground/Contents.swift
+++ Programming in Swift/Control Flow/05 - Challenge/PS_2-05_Start.playground/Contents.swift
@@ -35,15 +35,15 @@
  */
 let range = 1...10
 for i in range {
-    print(i*i)
+    let square = i*i
+    print(square)
 }
-print
-print(" --- --- --- ")
+print("--- --- ---")
 /*:
  The code below iterates over only even rows. Change this to use a where clause on the first for loop to skip even rows instead of using continue. Check that the sum is still 448 after your modifications.
  */
 var sum = 0
-for row in 0..<8 where row % 2 != 0 {
+for row in 0..<8 where row % 2 == 1 {
     print("Row is \(row)")
   for column in 0..<8 {
     print("Column is \(column)")
@@ -51,4 +51,4 @@ for row in 0..<8 where row % 2 != 0 {
     print("Sum is \(sum)")
   }
 }
-sum
+sum == 448
