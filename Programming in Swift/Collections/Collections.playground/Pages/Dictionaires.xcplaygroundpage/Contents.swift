a5acf03e4d867afbe6a89cc11086f505dc000ffe
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-05-15 22:57:44 -0700

    intro to dictionaries

---
+++ Programming in Swift/Collections/Collections.playground/Pages/Dictionaires.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+// Dictionaries
+// Contain Keys and Values
+// Keys must be unique (i.e. pass the vertical line test)
+// Learning dictionaries and arrays together might not be ideal, according to Erica Sadun
+// Keys can be like any type
+    // Array indexes must be Ints
+// Keys have no order
+    // Indexes must be sequential
+
+// empty dictionary
+// types must be explicitly stated for empty dictionaries
+var emptyDictionary: [String: Int] = [:]
+
+// type inference works otherwise
+var namesAndPets = [
+    "Ron" : "🐀",
+    "Rincewind" : "Luggage",
+    "Thor" : "Hammer",
+    "Goku" : "Flying Nimbus"
+]
+// prints in a different order than the one the KVP (key-value pairs were entered in)
+print(namesAndPets)
+
+// You can return values by providing the key
+// Key
+namesAndPets["Rincewind"]
+// Value
+namesAndPets["Hammer"]
+// Nonexistent key
+namesAndPets["Calvin"]
+
+// utilizing nil coalescing
+let calvinPet = namesAndPets["Calvin"] ?? "No pet for Calvin"
+
+namesAndPets.isEmpty
+namesAndPets.count
+
+// Replace keys
+// Method 1
+// namesAndPets.updateValue("Owl", forKey: "Ron")
+namesAndPets["Ron"] = "Owl"
+namesAndPets["Calvin"] = "Tiger"
+
+// namesAndPets.removeValue(forKey: "Goku")
+namesAndPets["Goku"] = nil
+print(namesAndPets)
+
+for (character, pet) in namesAndPets {
+    print("\(character) has a pet named \(pet)")
+}
+
+for (name, _) in namesAndPets {
+    print(name)
+}
+
+for pet in namesAndPets.values {
+    print(pet)
+}
+
