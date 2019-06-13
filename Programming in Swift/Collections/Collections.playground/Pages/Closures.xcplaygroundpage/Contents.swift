0abc2e8b3cca70d58e997821eb4f3582bf51e21e
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-06-12 19:55:31 -0700

    closures review

--- Programming in Swift/Collections/Collections.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ Programming in Swift/Collections/Collections.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -5,40 +5,52 @@
         // this allows you to access things within and outside of the closure
 
 // starter code
+// the Operate type takes two Ints and returns one Int
 typealias Operate = (Int, Int) -> (Int)
 
+// This function that complies with the "Operate" type takes the sum of two Ints
 func add(number1: Int, number2: Int) -> Int {
     return number1 + number2
 }
+
+// This function takes two Ints and an argument of type "Operate"
 func printResultOf(_ a: Int, _ b: Int, operation: Operate) {
+    // this takes the Ints the user provides and places them in a function that complies with the Operate type
     let result = operation(a, b)
+    // prints the result, based on how the two Ints are used, if at all
     print("Result is \(result)")
 }
+
+// takes two Ints and passes them again into the add function
 printResultOf(5, 3, operation: add)
 
 // Closure versions below
 
 // no argument labels are used
-//let multiply: (Int, Int) -> Int = { (a: Int, b: Int) -> Int in
-//    return a * b
-//}
+let multiply0: (Int, Int) -> Int = { (a: Int, b: Int) -> Int in
+    return a * b
+}
 
-let multiply: (Int, Int) -> Int = { (a, b) in
+// Shortened and relies on type inference
+let multiply1: (Int, Int) -> Int = { (a, b) in
     a * b
 }
 
-//let multiply: (Int, Int) -> Int = {
-//    $0 * $1
-//}
+// Removes the declared parameters and assumes the user will only type in the appropriate amount
+let multiply2: (Int, Int) -> Int = {
+    $0 * $1
+}
 
 // a and b are not enclosed in parentheses
+// the types are not explictly stated
 let divide: Operate = { a, b in
     return a / b
 }
 
-multiply(4, 2)
+// use the closure as you would with a function
+multiply2(4, 2)
 
-printResultOf(21, 2, operation: multiply)
+printResultOf(21, 2, operation: multiply1)
 printResultOf(21, 2, operation: divide)
 
 //printResultOf(40, 20, operation: { $0 - $1 })
@@ -53,6 +65,8 @@ let voidClosure: () -> Void = {
     print("Yay, Swift!")
 }
 
+voidClosure()
+
 //: Closures close over values!
 //////////////
 var count = 0
@@ -61,6 +75,7 @@ let incrementCount = {
 }
 /////////////
 
+// each time the closure is called, it refers to the current value of count
 incrementCount()
 incrementCount()
 incrementCount()
