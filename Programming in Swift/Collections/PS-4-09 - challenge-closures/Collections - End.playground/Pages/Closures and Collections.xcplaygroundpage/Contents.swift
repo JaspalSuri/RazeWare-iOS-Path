1e8ac9af4e419f2fc37d740c4908e746d1d0c215
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-06-17 15:53:05 -0700

    added Catie's solution to the flatMap for loop challenge
    - it uses the almond brace formatting that another user was using in their code
     to get over the type [String] issue, it extracts them once more in a nested for loop.

--- Programming in Swift/Collections/PS-4-09 - challenge-closures/Collections - End.playground/Pages/Closures and Collections.xcplaygroundpage/Contents.swift
+++ Programming in Swift/Collections/PS-4-09 - challenge-closures/Collections - End.playground/Pages/Closures and Collections.xcplaygroundpage/Contents.swift
@@ -1,15 +1,16 @@
+//: [Previous](@previous)
 //: # Closures & Collections
 var names = ["Zeus", "Poseidon", "Ares", "Demeter"]
 
 // sort() & sort(by:) - Sorts in place / mutates the original
 names.sort()
 names.sort(by: { (a, b) -> Bool in
-    a > b
+  a > b
 })
 
 // sorted() & sorted(by:) - Returns a new collection that is sorted
 let longToShortNames = names.sorted {
-    $0.count > $1.count
+  $0.count > $1.count
 }
 
 longToShortNames
@@ -27,21 +28,19 @@ let largePrices = prices.filter { $0 > 5 }
 // `filter` as a `for` loop
 var arrayForFilteredResults: [Double] = []
 for price in prices where price > 5 {
-    arrayForFilteredResults.append(price)
+  arrayForFilteredResults.append(price)
 }
 arrayForFilteredResults
 
 //: `map`
 let salePrices = prices.map { price -> Double in
-    return price * 0.9
+  return price * 0.9
 }
-
-let salePrices2 = prices.map { Double($0) * 0.9 }
 
 // `map` as a `for` loop
 var arrayForSalePrices: [Double] = []
 for price in prices {
-    arrayForSalePrices.append(price * 0.9)
+  arrayForSalePrices.append(price * 0.9)
 }
 arrayForSalePrices
 
@@ -55,7 +54,7 @@ let sum = prices.reduce(0, +)
 // `reduce` as a `for` loop - `array` version
 var doubleForSum = 0.0
 for price in prices {
-    doubleForSum += price
+  doubleForSum += price
 }
 doubleForSum
 
@@ -63,14 +62,14 @@ doubleForSum
 var stock = [1.50: 5, 10.00: 2, 4.99: 20, 2.30: 5, 8.19: 30]
 
 let stockSum = stock.reduce(into: []) { result, pair in
-    result.append(pair.key * Double(pair.value))
+  result.append(pair.key * Double(pair.value))
 }
 
 // `reduce` as a `for` loop - `dictionary` version
 var arrayForStockValues: [Double] = []
 for (price, quantity) in stock {
-    let value = price * Double(quantity)
-    arrayForStockValues.append(value)
+  let value = price * Double(quantity)
+  arrayForStockValues.append(value)
 }
 arrayForStockValues
 
@@ -79,51 +78,35 @@ arrayForStockValues
 let userInput = ["meow!", "15", "37.5", "seven"]
 
 let validInput = userInput.compactMap { input in
-    Int(input)
+  Int(input)
 }
 
 // `compactMap` as a `for` loop
 var arrayForValidInput: [Int] = []
 for input in userInput {
-    guard let input = Int(input) else {
-        continue
-    }
-    arrayForValidInput.append(input)
+  guard let input = Int(input) else {
+    continue
+  }
+  arrayForValidInput.append(input)
 }
 arrayForValidInput
 
 
 let arrayOfDwarfArrays = [
-    ["Sleepy", "Grumpy", "Doc"],
-    ["Thorin", "Nori"]
+  ["Sleepy", "Grumpy", "Doc"],
+  ["Thorin", "Nori"]
 ]
 
 // TODO: Use `flatMap` and `filter` to create a new array that only contains the dwarves with names starting after "M"
 let dwarvesAfterM = arrayOfDwarfArrays.flatMap { array -> [String] in
-    return array.filter { dwarf -> Bool in
-        dwarf > "M"
-    }
+  return array.filter { dwarf -> Bool in
+    dwarf > "M"
+  }
 }
 let sortedDwarves = dwarvesAfterM.sorted()
-print(sortedDwarves)
 
 
 // `flatMap` as a `for` loop - I've leave this one as a bonus challenge!
-// Solution by @CatieMeow
-// https://forums.raywenderlich.com/t/programming-in-swift-closures-and-collections-ray-wenderlich/47109/23?u=jsios
-var dwarvesAfterM2: [String] = []
-// flatMap
-for dwarfArray in arrayOfDwarfArrays
-{
-    // filter
-    var dwarves: [String] = []
-    for dwarf in dwarfArray
-    {
-        if dwarf > "M"
-        {
-            dwarves.append(dwarf)
-        }
-    }
-    
-    dwarvesAfterM2 += dwarves
-}
+
+
+//: [Next](@next)
